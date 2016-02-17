1fae8ceb57e5c3401406ed0e6deabbd6f4192361
unknown <9500143g@BO1028-6-09.ad.dcs.gla.ac.uk>
2016-02-17 16:50:42 +0000

    Added model file

---
+++ models.py
@@ -0,0 +1,39 @@
+class Event(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    time = models.DataField()
+    venue = models.models.ForeignKey(Venue)
+    media = models.URLField()
+
+
+    def __unicode__(self):  #For Python 2, use __str__ on Python 3
+        return self.name
+
+class Rating (models.Model):
+    user = models.models.ForeignKey(User)
+    performer = models.models.ForeignKey(Performer)
+    type = models.CharField(max_length=24)
+    Date = models.CharField(max_length=24)
+    Rating = models.IntegerField(max_length=2)
+
+class Performer (models.Model):
+    name = models.CharField(max_length=128)
+    username = models.CharField(max_length=64)
+    email = models.CharField(max_length=128)
+    paypal = models.CharField(max_length=128)
+    media = models.URLField()
+
+class Venue(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    address = models.CharField(max_length=256)
+    contact = models.CharField(max_length=256)
+    location = models.CharField(max_length=128)
+
+class Ticket(models.Model):
+    price = models.IntegerField(max_length=100000)
+    event = models.models.ForeignKey(Event)
+    sold = models.IntegerField(max_length=1)
+
+class User(models.Model)
+    username = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+    location = models.CharField(max_length=128)
