eba689f6fcef821683d03c19d5eb35ffe17294a0
ShinRa26 <grahamkeenan@hotmail.co.uk>
2016-02-24 17:02:59 +0000

    Added import line

--- Gigstop/app/views.py
+++ Gigstop/app/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-#from app.models import Performer, Event, Rating, Venue, Ticket, User, Like
+from app.models import Performer, Event, Rating, Venue, Ticket, User, Like
 
 def index(request):
 	context_dict = {}
