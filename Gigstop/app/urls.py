24518de1c6342118a48b4d953bd9986335db40f5
ShinRa26 <grahamkeenan@hotmail.com>
2016-03-01 19:08:30 +0000

    Added login and about pages. Mapped the urls and created their views

--- Gigstop/app/urls.py
+++ Gigstop/app/urls.py
@@ -2,4 +2,8 @@ from django.conf.urls import patterns, url
 from app import views
 
 urlpatterns = patterns('', 
-	url(r'^$', views.index, name='index'))
+	url(r'^$', views.index, name='index'),
+	url(r'^about/$', views.about, name='about'),
+	url(r'^login/$', views.login, name='login'),
+	url(r'^logout/$', views.logout, name='logout'),
+	)
--- Gigstop/app/views.py
+++ Gigstop/app/views.py
@@ -1,7 +1,39 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from app.models import Performer, Event, Rating, Venue, Ticket, User, Like
 
 def index(request):
 	context_dict = {}
-	return render(request, 'Gigstop/wall.html', context_dict)
+	return render(request, 'Gigstop/index.html', context_dict)
+
+def about(request):
+	return render(request, 'Gigstop/about.html', {})
+
+def login(request):
+	#POST
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('pass')
+		user = authenticate(username=username, password = password)
+
+		if user:
+			if user.is_active:
+				login(request, user)
+				return HttpResponseRedirect('/app/')
+			else:
+				return HttpResponse('Your Gigstop account is disabled')
+		else:
+			print "Invalid login details supplied {0}, {1}".format(username, password)
+			return HttpResponse('Invalid login details supplied')
+	#GET
+	else:
+		return render(request, 'Gigstop/login.html', {})
+
+@login_required
+def logout(request):
+	logout(request)
+
+	return HttpResponseRedirect('/app/')
+
