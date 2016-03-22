1bfba115e2faf71f6941541bf11df4686eb9fd06
Patrick Gorevan <patrick@gorevan.com>
2016-03-22 19:44:07 +0000

    Buttons for buying tickets

--- Gigstop/static/JS/gigstop-ajax.js
+++ Gigstop/static/JS/gigstop-ajax.js
@@ -5,7 +5,17 @@
                  $.get('/app/interested_band/', {event_id: eventid}, function(data){
 
                 });
+ 					var myInnerHtml = document.getElementById("savemessage").innerHTML;
+					var concat = bandName + " has been added to your favourites!!!";
+					document.getElementById("savemessage").innerHTML = concat;
 
     		});
+
+             $('.buyticket1').click(function(){
+                var eventid =this.value;
+                var page = $(this).attr('data-url');
+                $('.buyit').load(page);
+				return false
+              });
 
 			});
